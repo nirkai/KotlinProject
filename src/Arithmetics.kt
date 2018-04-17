0249b35b234797b4bbf311f3e7a49af821f6cc32
n k <kaizler@g.jct.ac.il>
2018-04-18 00:58:19 +0300

    Segments

--- src/Arithmetics.kt
+++ src/Arithmetics.kt
@@ -112,62 +112,30 @@ class Arithmetics {
                     "M=M-1\n"
         }
 
-        fun and(index: Int): String {
-            var index2 = index
-            return "\n@SP\n" +
-                    "A=M-1\n" +
-                    "D=M\n" +
-                    "A=A-1\n" +
-                    "D=M-D\n" +
-                    "@IF_TRUE" + index + "\n" +
-                    "D;JEQ\n" +
-                    "(IF_TRUE" + ++index2 + ")\n" +
-                    "D=0\n" +
-                    "@SP\n" +
-                    "A=M-1\n" +
-                    "A=A-1\n" +
-                    "M=D\n" +
-                    "@IF_FALSE" + index + "\n" +
-                    "0;JMP\n" +
-                    "(IF_TRUE" + index + ")\n" +
-                    "D=M\n" +
-                    "@IF_FALSE" + index + "\n" +
-                    "D;JEQ\n" +
-                    "D=-1\n" +
-                    "@SP\n" +
-                    "A=M-1\n" +
-                    "A=A-1\n" +
-                    "M=D\n" +
-                    "(IF_FALSE" + index + ")\n" +
-                    "@SP\n" +
-                    "M=M-1\n"
-            index = index2
+        fun and(): String{
+            return """
+@SP
+A=M
+A=A-1
+D=M
+A=A-1
+M=M&D
+@SP
+M=M-1
+"""
         }
 
-        fun or(index: Int): String {
-            return "\n@SP\n" +
-                    "A=M-1\n" +
-                    "D=M\n" +
-                    "A=A-1\n" +
-                    "D=D+M\n" +
-                    "@IF_TRUE" + index + "\n" +
-                    "D;JLT\n" +
-                    "D=0\n" +
-                    "@SP\n" +
-                    "A=M-1\n" +
-                    "A=A-1\n" +
-                    "M=D\n" +
-                    "@IF_FALSE" + index + "\n" +
-                    "0;JMP\n" +
-                    "(IF_TRUE" + index + ")\n" +
-                    "D=-1\n" +
-                    "@SP\n" +
-                    "A=M-1\n" +
-                    "A=A-1\n" +
-                    "M=D\n" +
-                    "(IF_FALSE" + index + ")\n" +
-                    "@SP\n" +
-                    "M=M-1\n"
+        fun or(): String{
+            return """
+@SP
+A=M
+A=A-1
+D=M
+A=A-1
+M=M|D
+@SP
+M=M-1
+"""
         }
 
         fun not(): String {
@@ -177,14 +145,14 @@ A=M-1
 D=-1
 D=D-M
 M=D
-        """
+"""
         }
 
-        fun push(arg1: String, arg2: String): String {
+       /* fun push(arg1: String, arg2: String): String {
             var outPut = ""
-            /*if (arg2.toDouble() >= 0)
+            *//*if (arg2.toDouble() >= 0)
         outPut = "\n@" + arg2 +"\n" + "D=A\n" + "@0\nA=M\nM=D\n"
-    else*/
+    else*//*
             //outPut = "\n" + "D=" + arg2 + "\n@0\nA=M\nM=D\n"
             outPut = "\n@" + arg2 + "\n" + "D=A\n" + "@0\nA=M\nM=D\n"
             outPut += moveSpStep()
@@ -198,6 +166,6 @@ M=D
                     "D=D+1\n" +
                     "@0\n" +
                     "M=D"
-        }
+        }*/
     }
 }
--- src/Targil1.kt
+++ src/Targil1.kt
@@ -1,239 +1,154 @@
-import com.sun.javafx.animation.TickCalculation.sub
 import java.io.File
-import java.io.InputStream
-import javax.management.Query.eq
+import java.io.FileWriter
+
+var lableNum = 0
 
 fun main(args: Array<String>) {
-    val inputStream  = File("vm\\SimpleOr.vm")
-    val lineList = mutableListOf<String>()
-    var hackProgramString = "@256\nD=A\n@0\nM=D"
-    val outStream = File("vm\\SimpleOr.asm")
-    var lableNum = 0
-    inputStream.bufferedReader().useLines { lines -> lines.forEach { lineList.add(it)} }
-    lineList.forEach{
-        var y: List<String> = it.split(" ")
-        var x = y.get(0)
-        y= y.subList(1,y.size)
-        when(x){
-            "\\"   -> ""
-            "push" -> hackProgramString += push(y[0],y[1])
-            "add"  -> hackProgramString += Arithmetics.add()
-            "sub"  -> hackProgramString += Arithmetics.sub()
-            "neg"  -> hackProgramString += Arithmetics.neg()
-            "eq"   -> hackProgramString += Arithmetics.eq(lableNum++)
-            "gt"   -> hackProgramString += Arithmetics.gt(lableNum++)
-            "lt"   -> hackProgramString += Arithmetics.lt(lableNum++)
-            "and"  -> hackProgramString += Arithmetics.and(++lableNum)
-            "or"   -> hackProgramString += Arithmetics.or(++lableNum)
-            "not"  -> hackProgramString += Arithmetics.not()
+    // var p = "C:\\Users\\ניר\\Documents\\מדעי המחשב\\שנה ג - תשעח\\סימסטר ב\\עקרונות שפות תכנה\\Exercises\\Targil2\\project 08\\FunctionCalls\\StaticsTest"
+
+    val sys = File("Sys.vm")
+    if (sys.isFile){
+        var outputFileName = "output"
+        File(System.getProperty("user.dir")).walk().forEach { tstFile ->
+            if (tstFile.isFile && tstFile.name.contains(".tst") && !tstFile.name.contains("VME.tst")){
+                outputFileName = tstFile.name
+            }
+        }
+        outputFileName = outputFileName.removeSuffix(".tst")
+        val outStream = FileWriter(outputFileName + ".asm")
+
+        outStream
+        outStream.write("@256\n" +
+                "D=A\n" +
+                "@0\n" +
+                "M=D\n" +
+                Flow.call("Sys.init", 0, "Sys"))
+        println("Print sys.vm file!!!")
+        var output = ""
+        //outStream.append(output)
+        val path = System.getProperty("user.dir")
+        println("\nWorking Directory = $path \n")
+        File(System.getProperty("user.dir")).walk().forEach { fileVm ->
+            if (fileVm.isFile && fileVm.name.contains(".vm")){
+                output = vmToAsm(fileVm)
+                outStream.append(output)
+            }
+        }
+        outStream.close()
+    }
+    else{
+        File(System.getProperty("user.dir")).walk().forEach { fileVm ->
+            var asmfile = fileVm.name.removeSuffix(".vm")
+            if (fileVm.isFile && fileVm.name.contains(".vm")){
+                var outStream = FileWriter(asmfile + ".asm")
+                outStream.write("@256\n" +
+                        "D=A\n" +
+                        "@0\n" +
+                        "M=D\n")
+                var output = vmToAsm(fileVm)
+                outStream.append(output)
+                outStream.close()
+            }
+        }
+    }
+
+
+    /*File("vm").walk().forEach { fileVm ->
+        var str = fileVm.toString()
+        if (fileVm.isFile) {
+            println("absolute file: " + fileVm.absoluteFile)
+            println("absolute path: " + fileVm.absolutePath)
+            println("name: " + fileVm.name)
         }
 
+
+
+        if (str.contains(".vm") == true) {
+            val inputStream = File(str)
+            var fileNameLable = fileVm.name
+            vmToAsm(fileVm)
+            if (fileVm.name == "Sys.vm")
+            //fileNameLable = fileNameLable.removePrefix(fileVm.name)
+            str = str.removeSuffix(".vm")
+            str += ".asm"
+            // val inputStream  = File("vm\\BasicTest.vm")
+            val lineList = mutableListOf<String>()
+            var hackProgramString = "@256\nD=A\n@0\nM=D\n"
+            val outStream = File(str)
+            // val outStream = File("vm\\BasicTest.asm")
+            var lableNum = 0
+            inputStream.bufferedReader().useLines { lines -> lines.forEach { lineList.add(it) } }
+            lineList.forEach {
+                it.toLowerCase()
+                var y: List<String> = it.split(" ")
+                var x = y.get(0)
+                y = y.subList(1, y.size)
+                when (x) {
+                    "\\" -> ""
+                    "label" -> hackProgramString += Flow.label(fileNameLable, y[0])
+                    "goto" -> hackProgramString += Flow.goto(fileNameLable, y[0])
+                    "if-goto" -> hackProgramString += Flow.ifGoto(fileNameLable, y[0])
+                    "call" -> hackProgramString += Flow.call(y[0],y[1].toInt(),fileNameLable)
+                    "function" -> hackProgramString += Flow.function(y[0],y[1])
+                    "return" -> hackProgramString += Flow.returnFunc()
+                    "push" -> hackProgramString += Segments.push(y[0], y[1],fileNameLable)
+                    "pop" -> hackProgramString += Segments.pop(y[0], y[1],fileNameLable)
+                    "add" -> hackProgramString += Arithmetics.add()
+                    "sub" -> hackProgramString += Arithmetics.sub()
+                    "neg" -> hackProgramString += Arithmetics.neg()
+                    "eq" -> hackProgramString += Arithmetics.eq(lableNum++)
+                    "gt" -> hackProgramString += Arithmetics.gt(lableNum++)
+                    "lt" -> hackProgramString += Arithmetics.lt(lableNum++)
+                    "and" -> hackProgramString += Arithmetics.and()
+                    "or" -> hackProgramString += Arithmetics.or()
+                    "not" -> hackProgramString += Arithmetics.not()
+                }
+            }
+            outStream.writeText(hackProgramString)
+
+        }
+    }*/
+}
+
+fun vmToAsm(fileVm: File) : String{
+    println(fileVm.absolutePath + " " + fileVm.name)
+    var str = fileVm.absolutePath
+    val inputStream = File(str)
+    var fileNameLable = fileVm.name.removeSuffix(".vm")
+    //str = str.removeSuffix(".vm")
+
+
+    val lineList = mutableListOf<String>()
+    var hackProgramString = ""
+
+
+    inputStream.bufferedReader().useLines { lines -> lines.forEach { lineList.add(it) } }
+    lineList.forEach {
+        it.toLowerCase()
+        var y: List<String> = it.split(" ", "\t")
+        var x = y.get(0)
+        y = y.subList(1, y.size)
+        when (x) {
+            "\\" -> ""
+            "label" -> hackProgramString += Flow.label(fileNameLable, y[0])
+            "goto" -> hackProgramString += Flow.goto(fileNameLable, y[0])
+            "if-goto" -> hackProgramString += Flow.ifGoto(fileNameLable, y[0])
+            "call" -> hackProgramString += Flow.call(y[0],y[1].toInt(),fileNameLable)
+            "function" -> hackProgramString += Flow.function(y[0],y[1])
+            "return" -> hackProgramString += Flow.returnFunc()
+            "push" -> hackProgramString += Segments.push(y[0], y[1],fileNameLable)
+            "pop" -> hackProgramString += Segments.pop(y[0], y[1],fileNameLable)
+            "add" -> hackProgramString += Arithmetics.add()
+            "sub" -> hackProgramString += Arithmetics.sub()
+            "neg" -> hackProgramString += Arithmetics.neg()
+            "eq" -> hackProgramString += Arithmetics.eq(lableNum++)
+            "gt" -> hackProgramString += Arithmetics.gt(lableNum++)
+            "lt" -> hackProgramString += Arithmetics.lt(lableNum++)
+            "and" -> hackProgramString += Arithmetics.and()
+            "or" -> hackProgramString += Arithmetics.or()
+            "not" -> hackProgramString += Arithmetics.not()
+        }
     }
-    outStream.writeText(hackProgramString)
+    return hackProgramString
 }
-
-
-
-/*
-fun add():String {
-    return "\n" +
-            "@SP\n" +
-            "A=M-1\n" +
-            "D=M\n" +
-            "A=A-1\n" +
-            "M=D+M\n" +
-            "@SP\n" +
-            "M=M-1\n"
-}
-
-fun sub(): String {
-    return "\n" +
-            "@SP\n" +
-            "A=M-1\n" +
-            "D=M\n" +
-            "A=A-1\n" +
-            "M=M-D\n" +
-            "@SP\n" +
-            "M=M-1\n"
-}
-
-fun neg(): String{
-    return "\n" +
-            "@SP\n" +
-            "A=M-1\n" +
-            "D=M\n" +
-            "@0\n" +
-            "D=A-D\n" +
-            "A=M-1\n" +
-            "M=D\n"
-
-}
-
-fun eq(index: Int): String {
-    return "\n@SP\n" +
-            "A=M-1\n" +
-            "D=M\n" +
-            "A=A-1\n" +
-            "D=D-M\n" +
-            "@IF_TRUE" + index+"\n" +
-            "D;JEQ\n" +
-            "D=0\n" +
-            "@SP\n" +
-            "A=M-1\n" +
-            "A=A-1\n" +
-            "M=D\n" +
-            "@IF_FALSE" + index+"\n" +
-            "0;JMP\n" +
-            "(IF_TRUE" + index+")\n" +
-            "D=-1\n" +
-            "@SP\n" +
-            "A=M-1\n" +
-            "A=A-1\n" +
-            "M=D\n" +
-            "(IF_FALSE" + index+")\n" +
-            "@SP\n" +
-            "M=M-1\n"
-}
-
-fun gt(index: Int): String {
-    return "@SP\n" +
-            "A=M-1\n" +
-            "D=M\n" +
-            "A=A-1\n" +
-            "D=M-D\n" +
-            "@IF_TRUE" + index+"\n" +
-            "D;JGT\n" +
-            "D=0\n" +
-            "@SP\n" +
-            "A=M-1\n" +
-            "A=A-1\n" +
-            "M=D\n" +
-            "@IF_FALSE" + index+"\n" +
-            "0;JMP\n" +
-            "(IF_TRUE" + index+")\n" +
-            "D=-1\n" +
-            "@SP\n" +
-            "A=M-1\n" +
-            "A=A-1\n" +
-            "M=D\n" +
-            "(IF_FALSE" + index+")\n" +
-            "@SP\n" +
-            "M=M-1"
-}
-
-fun lt(index: Int): String {
-    return "\n@SP\n" +
-            "A=M-1\n" +
-            "D=M\n" +
-            "A=A-1\n" +
-            "D=M-D\n" +
-            "@IF_TRUE" + index+"\n" +
-            "D;JLT\n" +
-            "D=0\n" +
-            "@SP\n" +
-            "A=M-1\n" +
-            "A=A-1\n" +
-            "M=D\n" +
-            "@IF_FALSE" + index+"\n" +
-            "0;JMP\n" +
-            "(IF_TRUE" + index+")\n" +
-            "D=-1\n" +
-            "@SP\n" +
-            "A=M-1\n" +
-            "A=A-1\n" +
-            "M=D\n" +
-            "(IF_FALSE" + index+")\n" +
-            "@SP\n" +
-            "M=M-1\n"
-}
-
-fun and(index:Int):String{
-    var index2 = index
-    return "\n@SP\n" +
-            "A=M-1\n" +
-            "D=M\n" +
-            "A=A-1\n" +
-            "D=M-D\n" +
-            "@IF_TRUE" + index +"\n" +
-            "D;JEQ\n" +
-            "(IF_TRUE" + ++index2 + ")\n" +
-            "D=0\n" +
-            "@SP\n" +
-            "A=M-1\n" +
-            "A=A-1\n" +
-            "M=D\n" +
-            "@IF_FALSE" + index+"\n" +
-            "0;JMP\n" +
-            "(IF_TRUE" + index+")\n" +
-            "D=M\n" +
-            "@IF_FALSE" + index + "\n" +
-            "D;JEQ\n" +
-            "D=-1\n" +
-            "@SP\n" +
-            "A=M-1\n" +
-            "A=A-1\n" +
-            "M=D\n" +
-            "(IF_FALSE" + index+")\n" +
-            "@SP\n" +
-            "M=M-1\n"
-    index = index2
-}
-
-fun or(index:Int):String{
-    return "\n@SP\n" +
-            "A=M-1\n" +
-            "D=M\n" +
-            "A=A-1\n" +
-            "D=D+M\n" +
-            "@IF_TRUE" + index+"\n" +
-            "D;JLT\n" +
-            "D=0\n" +
-            "@SP\n" +
-            "A=M-1\n" +
-            "A=A-1\n" +
-            "M=D\n" +
-            "@IF_FALSE" + index+"\n" +
-            "0;JMP\n" +
-            "(IF_TRUE" + index+")\n" +
-            "D=-1\n" +
-            "@SP\n" +
-            "A=M-1\n" +
-            "A=A-1\n" +
-            "M=D\n" +
-            "(IF_FALSE" + index+")\n" +
-            "@SP\n" +
-            "M=M-1\n"
-}
-
-fun not():String {
-    return """
-@SP
-A=M-1
-D=-1
-D=D-M
-M=D
-        """
-}
-*/
-
-fun push(arg1: String, arg2:String):String{
-    var outPut = ""
-    /*if (arg2.toDouble() >= 0)
-        outPut = "\n@" + arg2 +"\n" + "D=A\n" + "@0\nA=M\nM=D\n"
-    else*/
-        //outPut = "\n" + "D=" + arg2 + "\n@0\nA=M\nM=D\n"
-    outPut = "\n@" + arg2 +"\n" + "D=A\n" + "@0\nA=M\nM=D\n"
-    outPut+=moveSpStep()
-
-    return outPut
-}
-
-fun moveSpStep():String {
-    return "\n" +
-            "D=A\n" +
-            "D=D+1\n" +
-            "@0\n" +
-            "M=D"
-}
-
 
